1f24543f222560f0fa62bf68423adcbdd2a0f203
Harrison Greenwood <harrison.greenwood5@gmail.com>
2024-08-16 11:10:18 +0000

    templates set up and static css

--- id/views.py
+++ id/views.py
@@ -4,4 +4,6 @@ from django.views import generic
 from .models import User, Profile, Fungi, Safety, CommunityPost
 
 class PostList(generic.ListView):
-    model = CommunityPost
+    queryset = CommunityPost.objects.filter(status=2)
+    template_name = "id/index.html"
+    context_object_name = 'post_list'
